1811d96f832599a12da92ced63207dce841a0e2d
L. Jiang <l.jiang.1024@gmail.com>
2026-02-13 11:04:46 +0800

    feat: implement pandoc installation for Linux and macOS x86_64

--- env/pandoc.env.sh
+++ env/pandoc.env.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
-# https://github.com/jgm/pandoc/releases/download/3.9/pandoc-3.9-arm64-macOS.zip
 # Usage: bash pandoc.env.sh <pandoc-version>
 #    <pandoc-version> example:
 #      - 3.9
+#
+# https://github.com/jgm/pandoc/releases/download/3.9/pandoc-3.9-linux-amd64.tar.gz
+# https://github.com/jgm/pandoc/releases/download/3.9/pandoc-3.9-linux-arm64.tar.gz
+# https://github.com/jgm/pandoc/releases/download/3.9/pandoc-3.9-arm64-macOS.zip
+# https://github.com/jgm/pandoc/releases/download/3.9/pandoc-3.9-x86_64-macOS.zip
 
 if [ ! -f "${HOME:?}/Applications/pandoc-${1:?}/bin/pandoc" ]; then
   pandoc_version="${1:?}" && \
@@ -11,9 +15,27 @@ if [ ! -f "${HOME:?}/Applications/pandoc-${1:?}/bin/pandoc" ]; then
   rm    -r -f "${HOME:?}/Applications/pandoc-${pandoc_version}/" && \
   # download and extract
   case $(uname -s)-$(uname -m) in
-    Linux-x86_64   | Linux-amd64)  echo "$(date '+%Y-%m-%d %H:%M:%S %z') [ ERROR ] not implemented." ;;
-    Linux-aarch64  | Linux-arm64)  echo "$(date '+%Y-%m-%d %H:%M:%S %z') [ ERROR ] not implemented." ;;
-    Darwin-x86_64  | Darwin-amd64) echo "$(date '+%Y-%m-%d %H:%M:%S %z') [ ERROR ] not implemented." ;;
+    Linux-x86_64   | Linux-amd64)  \
+      (\
+        cd "${HOME}/Applications/" && \
+        curl -fLO "https://github.com/jgm/pandoc/releases/download/${pandoc_version:?}/pandoc-${pandoc_version:?}-linux-amd64.tar.gz" && \
+        tar -xzf "pandoc-${pandoc_version:?}-linux-amd64.tar.gz" && \
+        rm -r -f "pandoc-${pandoc_version:?}-linux-amd64.tar.gz" && \
+        mv       "pandoc-${pandoc_version:?}/" "pandoc-${pandoc_version:?}/");;
+    Linux-aarch64  | Linux-arm64)  \
+      (\
+        cd "${HOME}/Applications/" && \
+        curl -fLO "https://github.com/jgm/pandoc/releases/download/${pandoc_version:?}/pandoc-${pandoc_version:?}-linux-arm64.tar.gz" && \
+        tar -xzf "pandoc-${pandoc_version:?}-linux-arm64.tar.gz" && \
+        rm -r -f "pandoc-${pandoc_version:?}-linux-arm64.tar.gz" && \
+        mv       "pandoc-${pandoc_version:?}/" "pandoc-${pandoc_version:?}/");;
+    Darwin-x86_64  | Darwin-amd64) \
+      (\
+        cd "${HOME}/Applications/" && \
+        curl -fLO "https://github.com/jgm/pandoc/releases/download/${pandoc_version:?}/pandoc-${pandoc_version:?}-x86_64-macOS.zip" && \
+        unzip -q "pandoc-${pandoc_version:?}-x86_64-macOS.zip" && \
+        rm -r -f "pandoc-${pandoc_version:?}-x86_64-macOS.zip" && \
+        mv       "pandoc-${pandoc_version:?}-x86_64/" "pandoc-${pandoc_version:?}/");;
     Darwin-aarch64 | Darwin-arm64) \
       (\
         cd "${HOME}/Applications/" && \
